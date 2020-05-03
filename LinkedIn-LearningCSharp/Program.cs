50c76da0eddf2ec96c96c17b729ff9c9dd93f3c4
skara moosh <skaramoosh@rocketmail.com>
2020-05-04 02:21:44 +0400

    Add project files.

---
+++ LinkedIn-LearningCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinkedIn_LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var anything = Console.ReadLine(); 
+            Console.WriteLine(anything);
+        }
+    }
+}
