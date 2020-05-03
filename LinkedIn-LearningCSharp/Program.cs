b28e77c7f242e0c4ece7d8b0d81183eda8ea9052
skara moosh <skaramoosh@rocketmail.com>
2020-05-04 03:20:57 +0400

    simple while and if statement and data entry.

--- LinkedIn-LearningCSharp/Program.cs
+++ LinkedIn-LearningCSharp/Program.cs
@@ -6,9 +6,51 @@ namespace LinkedIn_LearningCSharp
     {
         static void Main(string[] args)
         {
+            var password = "";
+            
+            while (password != "secret")
+            {
 
-            var anything = Console.ReadLine(); 
-            Console.WriteLine(anything);
+                Console.WriteLine("Input password");
+                password = Console.ReadLine();
+
+                if (password == "secret")
+                {
+                    Console.WriteLine("that is the correct password");
+                }
+                else if (password != "secret")
+                {
+                    Console.WriteLine("Incorrect password");
+                }
+            }
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+             
+            Console.WriteLine("What month were you born?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Your name is {0}", name);
+            Console.WriteLine("Your age is {0}", age);
+            Console.WriteLine("Your month of birth is {0}", month);
+
+            if (month == "march")
+                {
+                    Console.WriteLine("You are an Aries.");
+                }
+
+            if (month == "April")
+            {
+                Console.WriteLine("You are an Taurus.");
+            }
+
+            if (month == "may")
+            {
+                Console.WriteLine("You are an Gemini.");
+            }
         }
     }
 }
