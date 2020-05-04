9334ae2a2cd93cfb14b4bf7d07e2a633eb465dad
skara moosh <skaramoosh@rocketmail.com>
2020-05-05 02:38:25 +0400

    Creating new TryAgain function to replace duplicated code.

--- LinkedIn-LearningCSharp/Program.cs
+++ LinkedIn-LearningCSharp/Program.cs
@@ -6,6 +6,8 @@ namespace LinkedIn_LearningCSharp
     {
         static void Main(string[] args)
         {
+            /*
+            
             for (int z = 1; z <= 5; z++)
             {
                 for (int i = 1; i <= 10; i++)
@@ -19,10 +21,10 @@ namespace LinkedIn_LearningCSharp
                 }
             }
             
+            */
             
             
-            
-            
+            /*
             var password = "";
             
             while (password != "secret")
@@ -37,16 +39,33 @@ namespace LinkedIn_LearningCSharp
                 }
             }
             Console.WriteLine("that is the correct password");
-
+            */
 
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
 
+            if(name == "") 
+            {
+                name = TryAgain();
+            }
+
             Console.WriteLine("How old are you?");
             var age = Console.ReadLine();
-             
+
+            if (age == "")
+            {
+                age = TryAgain();
+            }
+
+
             Console.WriteLine("What month were you born?");
             var month = Console.ReadLine();
+
+            if (month == "")
+            {
+                month = TryAgain();
+            }
+
 
             Console.WriteLine("Your name is {0}", name);
             Console.WriteLine("Your age is {0}", age);
@@ -66,6 +85,12 @@ namespace LinkedIn_LearningCSharp
             {
                 Console.WriteLine("You are an Gemini.");
             }
+        }
+
+        static string TryAgain()
+        {
+            Console.WriteLine("You didn't write anything. Please try again.");
+            return Console.ReadLine();
         }
     }
 }
